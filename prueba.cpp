34098fc0a9dc7e3f89ad19b516c813fec0d448c5
DerekSave <workderekceo@gmail.com>
2024-04-09 14:05:08 -0400

    agregue otra s

--- prueba.cpp
+++ prueba.cpp
@@ -7,7 +7,7 @@ void printCombination(int num1, int num2, int num3, int num4, int num5, int num6
     printf("%d %d %d %d %d %d\n", num1, num2, num3, num4, num5, num6);
 }
 
-// Función para generar todas las combinaciones de 6 elementos de un conjunto de 50 elementos s
+// Función para generar todas las combinaciones de 6 elementos de un conjunto de 50 elementos ss
 void generateCombinations() {
     for (int num1 = 1; num1 <= 44; ++num1) {
         for (int num2 = num1 + 1; num2 <= 45; ++num2) {
